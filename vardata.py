3dbc3047ad6c11714832cb627a9ad80cb2ee2d5b
John O'Connell <johnoconnell@Johns-MacBook-Pro.local>
2023-03-06 15:36:05 -0500

    added data scraping for game results

--- vardata.py
+++ vardata.py
@@ -1,7 +1,8 @@
 '''
 Defining variables to be used in the web scraping process
 '''
-
+ 
+        
 years = ['2010','2011','2012','2013','2014','2015','2016',
          '2017','2018','2019','2020','2021','2022']
 
@@ -34,7 +35,7 @@ columns = ['team_abbr',
     'points_for', 
     'yards', 
     'plays',
-    'yards_per_plbay',
+    'yards_per_play',
     'turnovers',
     'fumbles_lost',
     'first_downs' ,
